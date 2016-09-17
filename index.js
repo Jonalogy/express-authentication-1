641f70328a0ee9efef100ce22cb6905a9339321c
celinewsm <Celine@Celines-MacBook-Pro.local>
2016-09-17 20:07:43 +0800

    finishing logic without flash

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var app = express();
+var session = require('express-session');
+var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
+// render layouts
 app.use(ejsLayouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || "potato",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', require('./controllers/auth'));
+
+app.use(function(req, res, next) {
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile');
+});
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -16,8 +45,6 @@ app.get('/', function(req, res) {
 app.get('/profile', function(req, res) {
   res.render('profile');
 });
-
-app.use('/auth', require('./controllers/auth'));
 
 var server = app.listen(process.env.PORT || 3000);
 
